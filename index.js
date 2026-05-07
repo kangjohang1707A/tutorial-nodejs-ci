2232e690bc48a341fc79d4c7eac1a1c20042d643
kangjohang1707A <kangjohang1707a@gmail.com>
2026-05-07 20:18:20 +0700

    UPDATED: file index.js and deploy.yml

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 5000
-app.use(express.json())
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.send('hello from simple server hello world :)')
-})
+app.use(express.json());
 
-app.listen(port,"0.0.0.0", () => console.log('> Server is up and running on port : ' + port))
+app.get("/", (req, res) => {
+  res.send("hello from simple server hello world :)");
+});
+
+app.use((req, res, next) => {
+  const error = new Error("Not found");
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500).json({
+    message: error.message,
+  });
+});
+
+app.listen(port, "0.0.0.0", () =>
+  console.log("> Server is up and running on port : " + port),
+);
