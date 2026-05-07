0b41556f076a795a4ee3ba2384f97118e004f095
kangjohang1707A <kangjohang1707a@gmail.com>
2026-05-07 21:16:59 +0700

    yaml

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("hello world đã chỉnh sửa lại deploy.yml và docker-compose :)");
+  res.send("hello world tada :)");
 });
 
 app.use((req, res, next) => {
